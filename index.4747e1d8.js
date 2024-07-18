ae5f9a0371bca6662ccd8c6ac4552862ad276f8e
sheludkovladyslav <128414119+sheludkovladyslav@users.noreply.github.com>
2024-07-18 20:18:49 +0000

    Deploying to gh-pages from @ sheludkovladyslav/CUSH@2c95ccb62a4723fdb8778469f410c442fcdc6890 🚀

---
+++ index.4747e1d8.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[data-open_btn]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(function(){t.classList.toggle("menu-open"),document.body.classList.toggle("no-scroll")}))})();
+//# sourceMappingURL=index.4747e1d8.js.map
---
+++ index.8db2c6a6.js
@@ -0,0 +1,2 @@
+!function(){var e,t;e=document.querySelector("[data-open_btn]"),t=document.querySelector("[data-menu]"),e.addEventListener("click",(function(){t.classList.toggle("menu-open"),document.body.classList.toggle("no-scroll")}))}();
+//# sourceMappingURL=index.8db2c6a6.js.map
