ba51a23e1be16ae5444f2ceccf50ae27a461c320
Vladyslav Sheludko <vladsh2103@gmail.com>
2024-08-10 10:29:18 -0400

    Доробив календар

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -1,6 +1,9 @@
 const july = [
   [
-    { numberOfDay: 1, dayName: 'Понеділок' },
+    {
+      numberOfDay: 1,
+      dayName: 'Понеділок',
+    },
     { numberOfDay: 2, dayName: 'Понеділок' },
     { numberOfDay: 3, dayName: 'Понеділок' },
     { numberOfDay: 4, dayName: 'Понеділок' },
@@ -50,28 +53,347 @@ const july = [
   ],
 ];
 
+const julyMath = [
+  [
+    {
+      numberOfDay: 1,
+      dayName: 'Понеділок',
+      events: ['Урок #1'],
+    },
+    { numberOfDay: 2, dayName: 'Понеділок' },
+    {
+      numberOfDay: 3,
+      dayName: 'Понеділок',
+      events: ['Урок #2', 'Дедлайн ДЗ#1'],
+    },
+    { numberOfDay: 4, dayName: 'Понеділок' },
+    { numberOfDay: 5, dayName: 'Понеділок' },
+  ],
+  [
+    { numberOfDay: 1, dayName: 'Вівторок', events: ['Урок #3'] },
+    {
+      numberOfDay: 2,
+      dayName: 'Вівторок',
+      events: ['Дедлайн ДЗ#2'],
+    },
+    { numberOfDay: 3, dayName: 'Вівторок' },
+    {
+      numberOfDay: 4,
+      dayName: 'Вівторок',
+      events: ['Урок #4'],
+    },
+    { numberOfDay: 5, dayName: 'Вівторок' },
+  ],
+  [
+    { numberOfDay: 1, dayName: 'Середа', events: ['Урок #5', 'Дедлайн ДЗ#3'] },
+    { numberOfDay: 2, dayName: 'Середа' },
+    { numberOfDay: 3, dayName: 'Середа', events: ['Тестове НМТ'] },
+    { numberOfDay: 4, dayName: 'Середа' },
+    { numberOfDay: 5, dayName: 'Середа' },
+  ],
+  [
+    { numberOfDay: 1, dayName: 'Четвер' },
+    {
+      numberOfDay: 2,
+      dayName: 'Четвер',
+      events: ['Урок #6', 'Дедлайн ДЗ#4'],
+    },
+    { numberOfDay: 3, dayName: 'Четвер' },
+    { numberOfDay: 4, dayName: 'Четвер', events: ['Q&A #1'] },
+    { numberOfDay: 5, dayName: 'Четвер' },
+  ],
+  [
+    { numberOfDay: 1, dayName: "П'ятниця", events: ['Урок #7'] },
+    {
+      numberOfDay: 2,
+      dayName: "П'ятниця",
+      events: ['Дедлайн ДЗ#5'],
+    },
+    { numberOfDay: 3, dayName: "П'ятниця" },
+    { numberOfDay: 4, dayName: "П'ятниця" },
+    {
+      numberOfDay: 5,
+      dayName: "П'ятниця",
+      events: ['Q&A #2'],
+    },
+  ],
+  [
+    { numberOfDay: 1, dayName: 'Субота' },
+    { numberOfDay: 2, dayName: 'Субота', events: ['Дедлайн ДЗ#6'] },
+    { numberOfDay: 3, dayName: 'Субота' },
+    { numberOfDay: 4, dayName: 'Субота' },
+    { numberOfDay: 5, dayName: 'Субота' },
+  ],
+  [
+    {
+      numberOfDay: 1,
+      dayName: 'Неділя',
+      events: ['Як вибрати спеціальність?'],
+    },
+    { numberOfDay: 2, dayName: 'Неділя' },
+    { numberOfDay: 3, dayName: 'Неділя' },
+    { numberOfDay: 4, dayName: 'Неділя' },
+    { numberOfDay: 5, dayName: 'Неділя' },
+  ],
+];
+
+const julyEnglish = [
+  [
+    { numberOfDay: 1, dayName: 'Понеділок', events: ['Урок #1'] },
+    { numberOfDay: 2, dayName: 'Понеділок' },
+    {
+      numberOfDay: 3,
+      dayName: 'Понеділок',
+      events: ['Урок #3', 'Дедлайн ДЗ#2'],
+    },
+    {
+      numberOfDay: 4,
+      dayName: 'Понеділок',
+      events: ['Дедлайн ДЗ#3', 'Урок #4'],
+    },
+    { numberOfDay: 5, dayName: 'Понеділок', events: ['Урок #5'] },
+  ],
+  [
+    { numberOfDay: 1, dayName: 'Вівторок' },
+    {
+      numberOfDay: 2,
+      dayName: 'Вівторок',
+      events: ['Урок #6', 'Дедлайн ДЗ#4'],
+    },
+    {
+      numberOfDay: 3,
+      dayName: 'Вівторок',
+      events: ['Урок #7'],
+    },
+    { numberOfDay: 4, dayName: 'Вівторок' },
+    {
+      numberOfDay: 5,
+      dayName: 'Вівторок',
+      events: ['Екзамен #1'],
+    },
+  ],
+  [
+    {
+      numberOfDay: 1,
+      dayName: 'Середа',
+      events: ['Урок #8'],
+    },
+    {
+      numberOfDay: 2,
+      dayName: 'Середа',
+      events: ['Урок #9'],
+    },
+    { numberOfDay: 3, dayName: 'Середа' },
+    {
+      numberOfDay: 4,
+      dayName: 'Середа',
+      events: ['Практика #1'],
+    },
+    { numberOfDay: 5, dayName: 'Середа' },
+  ],
+  [
+    { numberOfDay: 1, dayName: 'Четвер' },
+    {
+      numberOfDay: 2,
+      dayName: 'Четвер',
+      events: ['Практика #2', 'Дедлайн ДЗ#5'],
+    },
+    {
+      numberOfDay: 3,
+      dayName: 'Четвер',
+      events: ['Урок #10'],
+    },
+    { numberOfDay: 4, dayName: 'Четвер' },
+    { numberOfDay: 5, dayName: 'Четвер' },
+  ],
+  [
+    { numberOfDay: 1, dayName: "П'ятниця" },
+    {
+      numberOfDay: 2,
+      dayName: "П'ятниця",
+      events: ['Урок #11', 'Дедлайн ДЗ#6'],
+    },
+    { numberOfDay: 3, dayName: "П'ятниця" },
+    { numberOfDay: 4, dayName: "П'ятниця" },
+    {
+      numberOfDay: 5,
+      dayName: "П'ятниця",
+      events: ['Екзамен #2'],
+    },
+  ],
+  [
+    { numberOfDay: 1, dayName: 'Субота' },
+    { numberOfDay: 2, dayName: 'Субота' },
+    { numberOfDay: 3, dayName: 'Субота', events: ['Екзамен #3'] },
+    { numberOfDay: 4, dayName: 'Субота' },
+    { numberOfDay: 5, dayName: 'Субота' },
+  ],
+  [
+    { numberOfDay: 1, dayName: 'Неділя' },
+    { numberOfDay: 2, dayName: 'Неділя' },
+    { numberOfDay: 3, dayName: 'Неділя' },
+    { numberOfDay: 4, dayName: 'Неділя' },
+    { numberOfDay: 5, dayName: 'Неділя' },
+  ],
+];
+
+const julyChemistry = [
+  [
+    { numberOfDay: 1, dayName: 'Понеділок', events: ['Лекція #1'] },
+    {
+      numberOfDay: 2,
+      dayName: 'Понеділок',
+      events: ['Лекція #2'],
+    },
+    {
+      numberOfDay: 3,
+      dayName: 'Понеділок',
+      events: ['Дедлайн ДЗ#1', 'Лекція #3'],
+    },
+    { numberOfDay: 4, dayName: 'Понеділок', events: ['Експеримент'] },
+    {
+      numberOfDay: 5,
+      dayName: 'Понеділок',
+      events: ['Лекція #4'],
+    },
+  ],
+  [
+    { numberOfDay: 1, dayName: 'Вівторок' },
+    {
+      numberOfDay: 2,
+      dayName: 'Вівторок',
+      events: ['Лекція #5', 'Дедлайн ДЗ#2'],
+    },
+    { numberOfDay: 3, dayName: 'Вівторок', events: ['Практика'] },
+    {
+      numberOfDay: 4,
+      dayName: 'Вівторок',
+      events: ['Лекція #6'],
+    },
+    { numberOfDay: 5, dayName: 'Вівторок' },
+  ],
+  [
+    { numberOfDay: 1, dayName: 'Середа', events: ['Лекція #7'] },
+    { numberOfDay: 2, dayName: 'Середа' },
+    {
+      numberOfDay: 3,
+      dayName: 'Середа',
+      events: ['Дедлайн ДЗ#3'],
+    },
+    { numberOfDay: 4, dayName: 'Середа' },
+    {
+      numberOfDay: 5,
+      dayName: 'Середа',
+      events: ['Лабораторна робота'],
+    },
+  ],
+  [
+    {
+      numberOfDay: 1,
+      dayName: 'Четвер',
+      events: ['Лекція #8'],
+    },
+    {
+      numberOfDay: 2,
+      dayName: 'Четвер',
+      events: ['Дедлайн ДЗ#4'],
+    },
+    { numberOfDay: 3, dayName: 'Четвер' },
+    { numberOfDay: 4, dayName: 'Четвер' },
+    {
+      numberOfDay: 5,
+      dayName: 'Четвер',
+      events: ['Огляд тем'],
+    },
+  ],
+  [
+    { numberOfDay: 1, dayName: "П'ятниця" },
+    {
+      numberOfDay: 2,
+      dayName: "П'ятниця",
+      events: ['Лекція #9', 'Дедлайн ДЗ#5'],
+    },
+    { numberOfDay: 3, dayName: "П'ятниця" },
+    { numberOfDay: 4, dayName: "П'ятниця" },
+    {
+      numberOfDay: 5,
+      dayName: "П'ятниця",
+      events: ['Кінцевий тест'],
+    },
+  ],
+  [
+    { numberOfDay: 1, dayName: 'Субота' },
+    { numberOfDay: 2, dayName: 'Субота' },
+    { numberOfDay: 3, dayName: 'Субота', events: ['Лабораторна робота'] },
+    { numberOfDay: 4, dayName: 'Субота' },
+    { numberOfDay: 5, dayName: 'Субота' },
+  ],
+  [
+    { numberOfDay: 1, dayName: 'Неділя' },
+    { numberOfDay: 2, dayName: 'Неділя' },
+    { numberOfDay: 3, dayName: 'Неділя' },
+    { numberOfDay: 4, dayName: 'Неділя' },
+    { numberOfDay: 5, dayName: 'Неділя' },
+  ],
+];
+
+const courseSelector = document.getElementById('course-selector');
 const calendarCards = document.querySelector('.table__cards');
 
-calendarCards.innerHTML = july
-  .map(
-    dayArray =>
-      `<div class="table__day">
+function renderCalendar(july) {
+  calendarCards.innerHTML = july
+    .map(
+      dayArray =>
+        `<div class="table__day">
          ${dayArray
            .map(
              day =>
                `<div class="table__card">
+                <div class = "table__text">
                   <p class="table__numberofday">${day.numberOfDay}</p>
                   <p class="table__nameofday ${
                     day.numberOfDay === 1 ? 'table__nameofday--first' : ''
                   }">
                     ${day.dayName}
                   </p>
-                  <p class="table__nameofevent">${
-                    day.event ? day.event : ''
-                  }</p>
-                </div>`
+                  </div>
+                  <div class="table__events">
+                  ${
+                    day.events && day.events.length > 0
+                      ? day.events
+                          .map(
+                            event =>
+                              `<p class="table__nameofevent">${event}</p>`
+                          )
+                          .join('')
+                      : ''
+                  }
+                </div>
+                </div>
+            `
            )
            .join('')}
        </div>`
-  )
-  .join('');
+    )
+    .join('');
+}
+
+renderCalendar(july);
+
+courseSelector.addEventListener('change', function () {
+  const selectedCourse = this.value;
+
+  switch (selectedCourse) {
+    case 'math':
+      renderCalendar(julyMath);
+      break;
+    case 'english':
+      renderCalendar(julyEnglish);
+      break;
+    case 'chemistry':
+      renderCalendar(julyChemistry);
+      break;
+    default:
+      renderCalendar(julyDefault);
+  }
+});
