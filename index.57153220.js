fa0642e6635f8e62cdeceb2050681de47a5daaa4
sheludkovladyslav <128414119+sheludkovladyslav@users.noreply.github.com>
2024-08-11 06:57:27 +0000

    Deploying to gh-pages from @ sheludkovladyslav/CUSH@7009f5b1fb10a22636a387a80736919f851275cd 🚀

---
+++ index.57153220.js
@@ -0,0 +1,2 @@
+document.querySelector(".mailing__btn").addEventListener("click",(function(){alert("Ви успішно підписались на розсилку!")}));
+//# sourceMappingURL=index.57153220.js.map
---
+++ index.f7be50a3.js
@@ -0,0 +1,2 @@
+document.querySelector(".mailing__btn").addEventListener("click",(()=>{alert("Ви успішно підписались на розсилку!")}));
+//# sourceMappingURL=index.f7be50a3.js.map
