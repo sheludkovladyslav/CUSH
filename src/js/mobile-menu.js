373faa1afed84f8e9f216e0725598de4d1709611
Vladyslav Sheludko <vladsh2103@gmail.com>
2024-08-13 00:55:59 -0400

    Доробив фікс

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -7,4 +7,13 @@
     menu.classList.toggle('menu-open');
     document.body.classList.toggle('no-scroll');
   }
+
+  function closeOnLink(event) {
+    if (event.target.classList.contains('links__link')) {
+      menu.classList.remove('menu-open');
+      document.body.classList.remove('no-scroll');
+    }
+  }
+
+  menu.addEventListener('click', closeOnLink);
 })();
